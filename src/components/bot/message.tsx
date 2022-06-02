888c0fe1045d18960f577ad83e8b298e2d07dd6c
Samuel Molinari <samuel@molinari.me>
2022-06-02 11:59:40 +0100

    End vegan and vegetarian conversation trees

--- src/components/bot/message.tsx
+++ src/components/bot/message.tsx
@@ -1,14 +1,14 @@
 import React from "react"
 import { PlayIcon } from "@heroicons/react/solid"
 
-const OptionalLinkedText = ({ text, url }) => {
+const OptionalLinkedText = ({ children, url }) => {
   if (url) {
     return (
-      <a href={url} target="_blank" className="underline">{text}</a>
+      <a href={url} target="_blank" className="underline">{children}</a>
     )
   }
 
-  return text
+  return children
 }
 
 export default ({ text, url, image, youtubeVideoId, startTime, them, typing }) => (
@@ -16,8 +16,8 @@ export default ({ text, url, image, youtubeVideoId, startTime, them, typing }) =
     <div className={`rounded-xl ${them ? "bg-gray-300" : (typing ? "bg-red-300" : "bg-red-500")}`}>
       {youtubeVideoId ? (
         <a className="relative block" target="_blank" href={`https://www.youtube.com/watch?v=${youtubeVideoId}${startTime ? "&t=" + startTime : ""}`}>
-          <div className="opacity-50 hover:opacity-75 absolute top-0 bottom-0 left-0 right-0 z-10 flex justify-center items-center">
-            <PlayIcon className="w-[40%] text-white" />
+          <div className="opacity-75 hover:opacity-100 absolute top-0 bottom-0 left-0 right-0 z-10 flex justify-center items-center">
+            <PlayIcon className="w-[40%] text-white drop-shadow-md" />
           </div>
           <img
             src={`http://img.youtube.com/vi/${youtubeVideoId}/0.jpg`}
@@ -25,10 +25,10 @@ export default ({ text, url, image, youtubeVideoId, startTime, them, typing }) =
           />
         </a>
       ) : null}
-      {image ? <img className="w-full rounded-t-xl" src={image} /> : null}
+      {image ? <OptionalLinkedText url={url}><img className="w-full rounded-t-xl" src={image} /></OptionalLinkedText> : null}
       {(text || typing) ? (
         <p className={`${them ? "text-gray-500" : "text-white"} py-2 px-4 font-medium text-md`}>
-          {typing ? (<>&nbsp;&nbsp;<span className="dot-flashing"></span>&nbsp;&nbsp;</>) : <OptionalLinkedText url={url} text={text} />}
+          {typing ? (<>&nbsp;&nbsp;<span className="dot-flashing"></span>&nbsp;&nbsp;</>) : <OptionalLinkedText url={url}>{text}</OptionalLinkedText>}
         </p>
       ) : null}
     </div>
